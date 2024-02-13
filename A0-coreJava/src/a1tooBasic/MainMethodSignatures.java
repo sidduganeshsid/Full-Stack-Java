eff6052cb95ecd6d65bc9c8c7d1accafb3f3f767
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-02-13 19:16:57 +0530

    core Java

---
+++ A0-coreJava/src/a1tooBasic/MainMethodSignatures.java
@@ -0,0 +1,16 @@
+package a1tooBasic;
+
+public class MainMethodSignatures {
+
+//	public static void main(String[] args) {
+//	static public void main(String[] args) {
+//	public static void main(String[] harry) {
+//	public static void main(String[] ...args) {
+//	public static void main(String...args) {
+//	public static void main(String... args) {
+//	final public static void main(String args[]) {
+	
+	public static void main(String args[]) {
+		System.out.println("HelloWorld");
+	}
+}
